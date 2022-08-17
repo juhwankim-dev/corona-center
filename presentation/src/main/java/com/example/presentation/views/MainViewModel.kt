2fbfd4997e5187293950262736ac55a7d86a6664
juhwankim-dev <juhwan.dev@gmail.com>
2022-08-17 20:30:45 +0900

    feat: :sparkles: presentation 레이어 기능 구현 - viewModel 구현(#2)

---
+++ presentation/src/main/java/com/example/presentation/views/MainViewModel.kt
@@ -0,0 +1,99 @@
+package com.example.presentation.views
+
+import androidx.lifecycle.*
+import com.example.domain.model.Center
+import com.example.domain.usecase.DeleteAllCentersUseCase
+import com.example.domain.usecase.GetCenterListUseCase
+import com.example.domain.usecase.ReadCenterListUseCase
+import com.example.domain.usecase.WriteCenterListUseCase
+import com.example.domain.util.Result
+import com.example.domain.util.Status
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getCenterListUseCase: GetCenterListUseCase,
+    private val readCenterListUseCase: ReadCenterListUseCase,
+    private val writeCenterListUseCase: WriteCenterListUseCase,
+    private val deleteAllCentersUseCase: DeleteAllCentersUseCase
+): ViewModel() {
+    private val _problem = MutableLiveData<Result<Any>>()
+    val problem: LiveData<Result<Any>> get() = _problem
+
+    private val _centerList = MutableLiveData<List<Center>>()
+    val centerList: LiveData<List<Center>> get() = _centerList
+
+    // 이 값을 통해 10개의 페이지의 저장 진행률을 나타냄
+    // ex) 3 -> 30%, 10 -> 100%
+    private val _progressCount = MutableLiveData(0)
+
+    // 가장 먼저 저장된 db를 삭제하고 작업 시작
+    init {
+        deleteAllCenters()
+    }
+
+    private fun getCenterList() {
+        for(page in 1..10) {
+            viewModelScope.launch {
+                getCenterListUseCase(page)
+                    .onStart { showProgress() }
+                    .onCompletion { hideProgress() }
+                    .catch { _problem.postValue(Result.fail()) }
+                    .collect {
+                        if(it.isEmpty()) {
+                            _problem.postValue(Result.error("Response is empty", it))
+                        } else {
+                            _centerList.postValue(it)
+                            _problem.postValue(Result.success(it))
+                            writeCenterList(it)
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun readCenterList(): LiveData<List<Center>> {
+        return readCenterListUseCase().asLiveData()
+    }
+
+    private fun writeCenterList(centerList: List<Center>) {
+        viewModelScope.launch {
+            val result = writeCenterListUseCase(centerList)
+            if(result.status == Status.ERROR) {
+                _problem.postValue(result)
+            } else {
+                // 저장을 성공적으로 완료하면 진행률을 나타내는 값을 +1 한다.
+                _progressCount.value = _progressCount.value?.plus(1)
+            }
+        }
+    }
+
+    private fun deleteAllCenters() {
+        viewModelScope.launch {
+            val result = deleteAllCentersUseCase()
+            if(result.status == Status.ERROR) {
+                _problem.postValue(result)
+            } else {
+                getCenterList()
+            }
+        }
+    }
+
+    private fun showProgress() {
+
+    }
+
+    private fun hideProgress() {
+
+    }
+
+    companion object {
+        private const val TAG = "MainViewModel_Corona"
+    }
+}
